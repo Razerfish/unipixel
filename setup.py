232aaf246751b4c9edadc57516861b9650347ec6
razerfish <fiona@razerfish.dev>
2020-01-23 13:34:05 -0500

    Setup setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="unipixel-razerfish",
+    version="0.0.1",
+    author="Fiona Wilson",
+    author_email="fiona@razerfish.dev",
+    description="A testing utility for applications that would use the adafruit-circuitpython-neopixel package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/razerfish/unipixel",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.7"
+    ],
+    python_requires=">=3.7"
+)
