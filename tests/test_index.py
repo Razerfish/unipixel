9c754b08b7948f83e1ddd320edb8e0f28b2ff677
razerfish <fiona@razerfish.dev>
2020-03-06 09:29:09 -0500

    Create test_get_slice in test_index.py

--- tests/test_index.py
+++ tests/test_index.py
@@ -201,3 +201,42 @@ def test_set_slice(test_strip, capsys):
             out, _ = capsys.readouterr()
 
             assert out == reference_string
+
+
+def test_get_slice(test_strip):
+    test_strip, params = test_strip
+
+    if params["pixel_order"] == unipixel.RGB:
+        test_data = resources.index_rgb
+
+    elif params["pixel_order"] == unipixel.GRB:
+        test_data = resources.index_grb
+
+    elif params["pixel_order"] == unipixel.RGBW:
+        test_data = resources.index_rgbw["input"]
+
+    elif params["pixel_order"] == unipixel.GRBW:
+        test_data = resources.index_grbw
+
+    elif params["pixel_order"] is None:
+        test_data = resources.index_grbw
+
+    else:
+        if not isinstance(params["pixel_order"], tuple) and params["pixel_order"] is not None:
+            raise TypeError("pixel_order must be a tuple or None")
+        raise ValueError("Unknown pixel order")
+
+    if params["pixel_order"] is None and params["bpp"] < 4:
+        case = pytest.raises(ValueError)
+    else:
+        case = utils.nullcontext()
+
+    with case:
+        for i in range(len(test_strip)):
+            test_strip[i] = test_data[i]
+
+        assert test_strip[1:15] == test_data[1:15]
+        assert test_strip[0:-1] == test_data[0:-1]
+        assert test_strip[0:7] == test_data[0:7]
+        assert test_strip[8:15] == test_data[8:15]
+        assert test_strip[8:-1] == test_data[8:-1]
